39b4be35502b885987a73397e4fc555f977fcc81
Shakti4640 <58823067+Shakti4640@users.noreply.github.com>
2024-01-09 20:00:51 +0530

    Create 0001 Easy 1 Two Sum.py

---
+++ Leetcode Top Interview Questions/Codes_here/0001 Easy 1 Two Sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+class Solution:
+    def twoSum(self, nums, target):
+        nums_hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in nums_hash: 
+                return [nums_hash[target - nums[i]], i]
+            nums_hash[nums[i]] = i
